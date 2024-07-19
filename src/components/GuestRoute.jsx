85fa4d37466dc5ea8fc578f92182d65ea089bcd4
Prince Gupta <121662994+Prince0207200@users.noreply.github.com>
2024-07-19 07:09:52 +0530

    Add files via upload

---
+++ src/components/GuestRoute.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { IS_AUTHENTICATED } from "../constants";
+
+function GuestRoute() {
+  const isAuth = IS_AUTHENTICATED;
+    // const isAuth = false;
+
+
+  if (isAuth) return <Navigate to="/" />;
+
+  return <Outlet/>;
+}
+
+export default GuestRoute;
